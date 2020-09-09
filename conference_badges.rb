9ac745a51ccc8fc4dfc5841a7711854808be307a
Eli Huebner <elimhuebner@gmail.com>
2020-09-09 18:00:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,8 +5,9 @@ end
 def batch_badge_creator(attendees)
   badge_batch = []
   batch_badge_creator.each do |speaker|
-    badge_batch.
+    badge_batch.push (badge_maker(speaker))
   end
+  badge_batch
 end
 
 def assign_rooms(attendees)
