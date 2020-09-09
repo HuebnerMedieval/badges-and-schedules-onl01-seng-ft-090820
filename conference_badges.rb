6ec187fdd058efe0096da084ce341ad250edc621
Eli Huebner <elimhuebner@gmail.com>
2020-09-09 18:12:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,5 +21,10 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  
+  batch_badge_creator(attendees).each do |speaker_badge|
+    puts speaker_badge
+  end
+  assign_rooms(attendees).each do |rooms|
+    puts rooms
+  end
 end
